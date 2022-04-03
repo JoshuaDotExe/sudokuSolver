5e79072185e44cba2b29736df30cb23dc3eae60c
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2022-04-03 18:40:30 +0100

    refactored sudoku.py
    remove simple single pencil mark solving method and replaced it with a finished naked set solving method, added extra premade games, added tests

--- app/lib/sudoku.py
+++ app/lib/sudoku.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 class sudoku:
     
@@ -51,9 +52,12 @@ class sudoku:
     # Constructor
     @classmethod
     def loadFromJSON(cls, fileName: str):
-        
         with open(f"app/resources/premadeGames/{fileName}.json", "r") as file:
             gameGrid = json.load(file)["grid"]
+        return sudoku.createGame(gameGrid)
+    
+    @classmethod
+    def createGame(cls, gameGrid: list):
         boardSize = len(gameGrid)
         
         ruleOptions = {
@@ -63,33 +67,29 @@ class sudoku:
         
         # Checks if the board is the correct size
         try:
-            classItem = cls(ruleOptions[boardSize])
+            game = cls(ruleOptions[boardSize])
         except ValueError:
             print(f"Board dimensions {boardSize} not supported")
             raise ValueError
         
-        classItem.grid = gameGrid
+        game.grid = gameGrid
         
         # Checks if all values in grid are valid for the grid size chosen
-        for row in classItem.grid:
+        for row in game.grid:
             if len(row) != int(boardSize):
                 print(f"Invalid board: Row {row} too long")
                 raise ValueError
             for item in row:
-                if item not in classItem.charSet and item != "0":
+                if item not in game.charSet and item != "0":
                     print(f"Invalid board: Item [{item}] is not a valid character")
                     raise ValueError
 
-        classItem.initPencilMarks()
-        classItem.removeBasicPencilMarks()
-        return classItem
-    
-    def initPencilMarks(self):
-        for rowIndex in range(self.gridSize):
-            for colIndex in range(self.gridSize):
-                if self.grid[rowIndex][colIndex] != '0':
-                    self.pencilMarks[rowIndex][colIndex] = []
-                    
+        game.removeBasicPencilMarks()
+        return game
+
+    # Grid interactions
+    # <----------------------------------------------------------------------->
+
     def verticalNeighbours(self, xCoord: int):
         returnList = []
         for row in self.grid:
@@ -113,6 +113,10 @@ class sudoku:
                 if item != "0":
                     returnList.append(item)
         return returnList
+
+    # <----------------------------------------------------------------------->
+    # Simple pencil marks interactions
+    # <----------------------------------------------------------------------->
     
     def pencilVerticalNeighbours(self, xCoord: int):
         returnList = []
@@ -174,60 +178,63 @@ class sudoku:
             returnList.append(itemList)
         
         return returnList
+
+    # <----------------------------------------------------------------------->
+    # Logic
+    # <----------------------------------------------------------------------->
+
+    def pencilNakedSet(self):
+        self.pencilNakedsHorizontal()
+        self.pencilNakedsVertical()
+        self.pencilNakedsBox()
         
-    def solveCrossHatchScanning(self, yCoord: int, xCoord: int) -> str:
-        for checkItem in self.pencilMarks[yCoord][xCoord]:
-            compareDict = self.pencilNeighboursDict(yCoord, xCoord)
-            if compareDict[checkItem] == 3:
-                print(f"{checkItem} valid in {yCoord}, {xCoord}")
-                return checkItem
-        return "0"
+    def pencilNakedsHorizontal(self):
+        for yCoord in range(self.gridSize):
+            self.nakedActions(self.pencilHorizontalNeighbours(yCoord))
+
+    def pencilNakedsVertical(self):
+        for xCoord in range(self.gridSize):
+            self.nakedActions(self.pencilVerticalNeighbours(xCoord))
+
+    def pencilNakedsBox(self):
+        for xNum in range(self.subgridSize):
+            xCoord = xNum * self.subgridSize
+            for yNum in range(self.subgridSize):
+                yCoord = yNum * self.subgridSize
+                self.nakedActions(self.pencilBoxNeighbours(yCoord, xCoord))
     
-    def pencilNakedPair(self, yCoord: int, xCoord: int):
-        curSpaceSet = set(self.pencilMarks[yCoord][xCoord])
-        spaceSetLength = len(curSpaceSet)
+    def nakedActions(self, house: list):
+        # Reminder, a house is a group of values should each be unique
+        houseTuples = list()
+        for item in house:
+            houseTuples.append(tuple(item))
+        similarMarks = dict.fromkeys(houseTuples, 0)
+        for item in houseTuples:
+            similarMarks[item] += 1
         
-        boxRow = (xCoord//3)*3
-        boxCol = (yCoord//3)*3
-        
-        counter = 0
-        for itemList in self.pencilBoxNeighbours(yCoord, xCoord):
-            if curSpaceSet == set(itemList):
-                counter += 1
-        
-        if counter != spaceSetLength:
-            return
-        for item in curSpaceSet:
-            self.removePencilMarksBox(item, xCoord, yCoord)
-        
-        self.reAddPencilMarks(curSpaceSet)
-        coordList = []
-        
-        
-        subYCount = 0
-        for row in self.pencilMarks[boxCol:boxCol+3]:
-            subXCount = 0
-            for item in row[boxRow:boxRow+3]:
-                if set(item) == curSpaceSet:
-                    coordList.append([subYCount, subXCount])
-                subXCount += 1
-            subYCount += 1
-        # Horizontal and vertical checkers arent working, pls fix :(
-        
-        # Checks if all horizontally alligned
-        if all(x[0] == coordList[0][0] for x in coordList) == True:
-            for item in curSpaceSet:
-                print(f"Attempting to remove {item} in row {yCoord}")
-                self.removePencilMarksHorizontal(item, yCoord)
-                self.reAddPencilMarks(curSpaceSet)
+        # Gets rid of the empty tuple which can really mess with stuff    
+        try:   
+            similarMarks.pop(())
+        except KeyError:
+            pass
+
+        for key, value in similarMarks.items():
+            # If equal it means the contents of the key can only be in
+            # said spaces and should be removed from the rest of the house
+            if len(key) == value:
+                for item in house:
+                    if tuple(item) != key:
+                        for symbol in key:
+                            try:
+                                item.remove(symbol)
+                            except ValueError:
+                                pass
             
-        
-        # Checks if all vertically alligned
-        if all(x[1] == coordList[0][1] for x in coordList) == True:
-            for item in curSpaceSet:
-                print(f"Attempting to remove {item} in column {xCoord}")
-                self.removePencilMarksVertical(item, xCoord)
-                self.reAddPencilMarks(curSpaceSet)
+    def reAddPencilMarks(self, workingSet : set):
+        for newYCoord in range(len(self.charSet)):
+            for newXCoord in range(len(self.charSet)):
+                if self.grid[newYCoord][newXCoord] == "0" and len(self.pencilMarks[newYCoord][newXCoord]) == 0:
+                    self.pencilMarks[newYCoord][newXCoord] = list(workingSet)
 
     def removePencilMarksVertical(self, target: str, xCoord: int):
         for row in self.pencilMarks:
@@ -264,32 +271,20 @@ class sudoku:
                     self.removePencilMarksVertical(item, xCoord)
                     self.pencilMarks[yCoord][xCoord] = []
 
-    def reAddPencilMarks(self, workingSet : set):
-        for newYCoord in range(len(self.charSet)):
-            for newXCoord in range(len(self.charSet)):
-                if self.grid[newYCoord][newXCoord] == "0" and len(self.pencilMarks[newYCoord][newXCoord]) == 0:
-                    self.pencilMarks[newYCoord][newXCoord] = list(workingSet)
-
+    # List of techniques
     def runTechniques(self):
-        self.nakedSingles()
+        self.pencilNakedSet()
+        self.loneSingles()
         
-        #for yCoord, row in enumerate(self.pencilMarks):
-        #    for xCoord, item in enumerate(row):
-                # Checks to see if it has a unique pencil mark in its box, row and column
-                #if len(self.pencilMarks[yCoord][xCoord]) > 1:
-                #    self.grid[yCoord][xCoord] = self.solveCrossHatchScanning(yCoord, xCoord)
-                # Runs naked pair technique
-                #if len(self.pencilMarks[yCoord][xCoord]) == 2:
-                #    self.pencilNakedPair(yCoord, xCoord)
 
     # Checks if only one pencil mark remains 
     # if true it replaces the grid space with it
-    def nakedSingles(self):
+    def loneSingles(self):
         for yCoord, row in enumerate(self.pencilMarks):
             for xCoord, item in enumerate(row):
                 if len(item) == 1:
                     self.grid[yCoord][xCoord] = item[0]
-                    self.removeBasicPencilMarks()
+        #self.removeBasicPencilMarks()
     
     # Iterates through grid
     # Finds if any spaces left to solve
@@ -301,15 +296,16 @@ class sudoku:
         return True        
         
     def solve(self):
+        for row in self.grid:
+            print(row)
         while self.solved == False:
-            for row in self.grid:
-                print(row)
-            for row in self.pencilMarks:
-                print(row)
-            print(f"Turn = {self.turnCounter}")
-            input()
+            #for row in self.grid:
+            #    print(row)
+            #for row in self.pencilMarks:
+            #    print(row)
+            #print(f"Turn = {self.turnCounter}")
+            #input()
             self.runTechniques()
-            
             self.turnCounter += 1
             
             self.solved = self.solvedCheck()
@@ -320,8 +316,10 @@ class sudoku:
             
         
 def main():
-    game = sudoku.loadFromJSON("easy")
+    game = sudoku.loadFromJSON("simple")
+    start = time.time_ns()
     game.solve()
-
+    end = time.time_ns()
+    print(f"Finished in {(end-start)/1000000}ms")
 if __name__ == "__main__":
     main()
