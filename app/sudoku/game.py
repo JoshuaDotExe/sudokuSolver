134daaf6694e68ff729ef32e4e86746135d16125
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2022-05-13 01:57:30 +0100

    added hidden sets solver and pointing sets solver
    updated the naked set solver to become the NH Set solver as I realised I can use the same function to solve for hidden sets

--- app/sudoku/game.py
+++ app/sudoku/game.py
@@ -16,12 +16,15 @@ logging.basicConfig(level=logging.INFO,
 logging.basicConfig(level=logging.WARNING,
                     handler=[logging.FileHandler("debug.log"),
                              logging.StreamHandler()])
+logging.basicConfig(level=logging.CRITICAL,
+                    handler=[logging.FileHandler("debug.log"),
+                             logging.StreamHandler()])
 
 class sudoku(markings):
     
-    def __init__(self, fileName="easy"):
+    def __init__(self, gameGrid):
         
-        self.grid = self.buildGridJSON(fileName)
+        self.grid = gameGrid
         self.gridSize = len(self.grid)
         self.subgridSize = round(self.gridSize**0.5) # Sqrt
         
@@ -90,12 +93,35 @@ class sudoku(markings):
         return printStr
     
     @staticmethod
-    def buildGridJSON(filename: str):
+    def buildGridJSON(filename = 'easy'):
         cwd = os.getcwd().replace("\\", "/") 
         targetDir = cwd + f"/app/resources/premadeGames/{filename}.json"
         with open(targetDir, "r") as file:
             gameGrid = json.load(file)["grid"]
-        return gameGrid
+        return sudoku(gameGrid)
+    
+    @staticmethod
+    def buildGridStr(inputStr: str):
+        strLength = len(inputStr)
+        strSqrt = round(strLength**0.5)
+        if strLength not in (16, 81, 256):
+            logging.critical('Board size not supported')
+            exit(ValueError)
+        counter = 0
+        gameGrid = []
+        for _ in range(strSqrt):
+            tempList = []
+            for item in inputStr[counter:counter+strSqrt]:
+                if item in ('0', '.', ' ', '*'):
+                    tempList.append('0')
+                    continue
+                tempList.append(item)
+            counter += strSqrt
+            gameGrid.append(tempList)
+        return sudoku(gameGrid)
+        
+        
+        
     
     def verticalNeighbours(self, xCoord: int):
         returnList = []
@@ -203,19 +229,19 @@ class sudoku(markings):
                     self.turnMoves += 1
         self.removeBasicMarks()
     
-    def nakedSets(self):
+    def nakedHiddenSets(self):
         for xCoord in range(self.gridSize):
-            if self.nakedSetsSolver(self.pencilVerticalNeighbours(xCoord), logDesc="vert", x=xCoord): return
+            if self.NHSetsSolver(self.pencilVerticalNeighbours(xCoord), logDesc="vert", x=xCoord): return
         for yCoord in range(self.gridSize):
-            if self.nakedSetsSolver(self.pencilHorizontalNeighbours(yCoord), logDesc="horz", y=yCoord): return
+            if self.NHSetsSolver(self.pencilHorizontalNeighbours(yCoord), logDesc="horz", y=yCoord): return
         boxCoords = [element*self.subgridSize for element in list(range(self.subgridSize))]
         for xCoord in boxCoords:
             for yCoord in boxCoords:
-                if self.nakedSetsSolver(self.pencilBoxNeighbours(yCoord, xCoord), logDesc="box", x=xCoord, y=yCoord): return
+                if self.NHSetsSolver(self.pencilBoxNeighbours(yCoord, xCoord), logDesc="box", x=xCoord, y=yCoord): return
     
     # Removes the found nakedSet from the non subset spaces
     @staticmethod
-    def nakedSetsEliminator(targetHouse: list, targets: set):
+    def NHSetsEliminator(targetHouse: list, targets: set):
         markElim = False
         for space in targetHouse:
             if frozenset(space).issubset(targets):
@@ -227,7 +253,7 @@ class sudoku(markings):
         return markElim
     
     @staticmethod
-    def nakedSetsDictBuilder(marksHouse: list):
+    def NHSetsDictBuilder(marksHouse: list):
         # Removes all empty spaces to make it easier to search for valid naked sets
         cleanedHouse = list(filter(lambda houseItem: len(houseItem) != 0, marksHouse))
         cleanedSets = [set(item) for item in cleanedHouse]
@@ -242,36 +268,152 @@ class sudoku(markings):
             for tempIndex in range(len(cleanedHouse)-1):
                 tempSet = deepcopy(markSet)
                 tempSet.update(set(cleanedHouse[tempIndex]))
-                if len(tempSet) > 4: continue
+                #if len(tempSet) > 4: continue
                 verboseSets.append(tempSet)
         frozenSets = [frozenset(item) for item in verboseSets]
         returnDict = dict.fromkeys(frozenSets, 0)
         return returnDict
     
-    def nakedSetsSolver(self, marksHouse: list, logDesc='NOTSET', x=-1, y=-1):
+    def NHSetsSolver(self, marksHouse: list, logDesc='NOTSET', x=-1, y=-1):
         marksHouseCopy = deepcopy(marksHouse)
         # Quick non comprehensive test if the house has been fully solved
         # running the solver with an item length of 3 also causes it to
         # see a naked pair when it's actually an unsolvable group of 3
         if all(len(item) <= 2 for item in marksHouseCopy): return False
         # Returns a dict of all possible mark combinations
-        possibleSets = self.nakedSetsDictBuilder(marksHouseCopy)
+        possibleSets = self.NHSetsDictBuilder(marksHouseCopy)
         # If there's only one grouping of marks the solver is useless
         for setItem in possibleSets.keys():
             for markItem in marksHouseCopy:
                 if markItem == []: continue
                 if set(markItem).issubset(setItem):
                     possibleSets[setItem] += 1
-        # Only looks for pairs, triples and quads, no quints
+        # Looks for naked pairs, triples and quads
         for numSet in (2, 3, 4):
             for setItem, setVal in possibleSets.items():
                 if len(setItem) == numSet and setVal == numSet:
-                    if self.nakedSetsEliminator(marksHouse, setItem) == False:
+                    if self.NHSetsEliminator(marksHouse, setItem) == False:
                         continue
                     self.turnMoves += 1
-                    logging.info(f'SOLVED | NAKED SET ({logDesc})| {tuple(setItem)} found at {f"X = {x}" if x >= 0 else ""}{" , " if x >= 0 and y >= 0 else ""}{f"Y = {y}" if y >= 0 else ""}')
+                    logging.info(f'SOLVED | NAKED SET ({logDesc}) | {tuple(setItem)} found at {f"X = {x}" if x >= 0 else ""}{" , " if x >= 0 and y >= 0 else ""}{f"Y = {y}" if y >= 0 else ""}')
+                    return True
+        # Looks for hidden pairs, triples and quads
+        for numSet in (7, 6, 5):
+            for setItem, setVal in possibleSets.items():
+                if len(setItem) == numSet and setVal == numSet:
+                    if self.NHSetsEliminator(marksHouse, setItem) == False:
+                        continue
+                    invertedSetItem = set(max(possibleSets.keys(), key=len)).difference(setItem)
+                    self.turnMoves += 1
+                    logging.info(f'SOLVED | HIDDEN SET ({logDesc}) | {tuple(invertedSetItem)} found at {f"X = {x}" if x >= 0 else ""}{" , " if x >= 0 and y >= 0 else ""}{f"Y = {y}" if y >= 0 else ""}')
                     return True
         return False
+    
+    def pointingSets(self):
+        for xCoord in range(self.gridSize):
+            self.pointingSetsVert(xCoord)
+        for yCoord in range(self.gridSize):
+            self.pointingSetsHorz(yCoord)
+        boxCoords = [element*self.subgridSize for element in list(range(self.subgridSize))]
+        for xCoord in boxCoords:
+            for yCoord in boxCoords:
+                self.pointingSetsBox(xCoord, yCoord)
+    
+    def pointingSetsEliminator(self, target: str, targetBL: list, marksHouse: list):
+        notableChange = False
+        for markIndex, markSpace in enumerate(marksHouse):
+            if markIndex in targetBL:
+                continue
+            if target in markSpace:
+                markSpace.remove(target)
+                notableChange = True
+        return notableChange
+    
+    def pointingSetsBox(self, x: int, y: int):
+        # If there's only 2 boxes left there's no pointing sets to solve
+        # and should be treated as a naked set instead
+        marksHouse = self.pencilBoxNeighbours(y, x)
+        if all(len(item) < 3 for item in marksHouse): return
+        
+        markDict = dict.fromkeys(self.charSet, 0)
+        for markSpace in marksHouse:
+            for item in markSpace:
+                markDict[item] += 1
+                
+        
+        for markKey, markVal in markDict.items():
+            # Passes the key if there's too many to get a pointing set
+            if markVal > self.subgridSize or markVal < 2:
+                continue
+            targetList = list()
+            for markIndex, markSpace in enumerate(marksHouse):
+                if markKey not in markSpace:
+                    continue
+                tempX = x + markIndex%self.subgridSize
+                tempY = y + markIndex//self.subgridSize
+                targetList.append((tempX, tempY))
+            if all(item[0] == targetList[0][0] for item in targetList) == True:
+                if self.pointingSetsEliminator(markKey, [item[1] for item in targetList], self.pencilVerticalNeighbours(tempX)):
+                    self.turnMoves += 1
+                    logging.info(f'SOLVED | POINTING SET (box-vert) | {markKey} found in column {targetList[0][0]} in spaces {tuple(item for item in targetList)}')
+                    
+            elif all(item[1] == targetList[0][1] for item in targetList) == True:
+                if self.pointingSetsEliminator(markKey, [item[0] for item in targetList], self.pencilHorizontalNeighbours(tempY)):
+                    self.turnMoves += 1
+                    logging.info(f'SOLVED | POINTING SET (box-horz) | {markKey} found in row {targetList[0][1]} in spaces {tuple(item for item in targetList)}')
+    
+    def pointingSetsVert(self, x: int):
+        marksHouse = self.pencilVerticalNeighbours(x)
+        if all(len(item) < 3 for item in marksHouse): return
+        
+        markDict = dict.fromkeys(self.charSet, 0)
+        for markSpace in marksHouse:
+            for item in markSpace:
+                markDict[item] += 1
+                
+        for markKey, markVal in markDict.items():
+            if markVal > self.subgridSize or markVal < 2:
+                continue
+            targetList = list()
+            for markIndex, markSpace in enumerate(marksHouse):
+                if markKey not in markSpace:
+                    continue
+                targetList.append((markIndex))
+            floorBase = targetList[0]//self.subgridSize
+            if all(item//self.subgridSize == floorBase for item in targetList) == False:
+                continue
+            
+            blackList = [item for item in range(x%self.subgridSize, self.gridSize, self.subgridSize)]
+            if self.pointingSetsEliminator(markKey, blackList, self.pencilBoxNeighbours(targetList[0], x)) == False:
+                continue
+            self.turnMoves += 1
+            logging.info(f'SOLVED | POINTING SET (vert) | {markKey} found in column {x} in spaces {tuple(f"({x}, {item})" for item in targetList)}')
+    
+    def pointingSetsHorz(self, y: int):
+        marksHouse = self.pencilHorizontalNeighbours(y)
+        if all(len(item) < 3 for item in marksHouse): return
+        
+        markDict = dict.fromkeys(self.charSet, 0)
+        for markSpace in marksHouse:
+            for item in markSpace:
+                markDict[item] += 1
+                
+        for markKey, markVal in markDict.items():
+            if markVal > self.subgridSize or markVal < 2:
+                continue
+            targetList = list()
+            for markIndex, markSpace in enumerate(marksHouse):
+                if markKey not in markSpace:
+                    continue
+                targetList.append(markIndex)
+            floorBase = targetList[0]//self.subgridSize
+            if all(item//self.gridSize == floorBase for item in targetList) == False:
+                continue
+            blackList = [item for item in range(floorBase, floorBase+self.subgridSize)]
+            if self.pointingSetsEliminator(markKey, blackList, self.pencilBoxNeighbours(y, targetList[0])) == False:
+                continue
+            self.turnMoves += 1
+            logging.info(f'SOLVED | POINTING SET (horz) | {markKey} found in row {y} in spaces{tuple(f" ({item}, {y})" for item in targetList)}')
     
     def methods(self):
         self.turnCounter += 1
@@ -280,8 +422,8 @@ class sudoku(markings):
         self.hiddenSingles()
         
         # If neither of the simple methods work
-        if self.turnMoves == 0:
-            self.nakedSets()
+        if self.turnMoves == 0: self.pointingSets()
+        if self.turnMoves == 0: self.nakedHiddenSets()
         
         if self.turnMoves == 0:
             self.solved = True
@@ -305,12 +447,12 @@ class sudoku(markings):
     def runPuzzle(self):
         print(f"Running Sudoku Solver : Version # {__version__}")
         print(textLogo)
+        print(self)
         while self.solved == False:
-            print(self)
-            input()
             self.methods()
         
 def main():
-    gameItem = sudoku()
+    
+    gameItem = sudoku.buildGridStr(input('Please enter a game string : '))
     gameItem.runPuzzle()
 
